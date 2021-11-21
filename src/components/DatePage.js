42fdd0c7fdefb3196599474937d2b65335d3d6af
Maten Rehimi <matenrehimi@hotmail.com>
2021-11-21 10:21:00 +0000

    refactor options

--- src/components/DatePage.js
+++ src/components/DatePage.js
@@ -1,55 +1,75 @@
-import React from 'react';
-import './styles/DatePage.css';
+import React from "react";
+import "./styles/DatePage.css";
 import TaskList from "./TaskList.js";
-import firebase from '../Firebase';
-
+import firebase from "../Firebase";
 
 class DatePage extends React.Component {
-
   //order of execution: constructor, componentWillMount, render, componentDidMount
   constructor(props) {
     super(props);
     const tempDate = this.props.location.state.selectedDate.split("/").join("");
     this.state = {
       date: tempDate,
-    }
+    };
   }
 
   getOptionPicked(event) {
-    const {date} = this.state;
-    firebase.database().ref("Calendar Page").child(date.slice(4,8)+"_"+date.slice(2,4)+"_"+date.slice(0,2)).child("Day Rating").set(event.target.value)
+    const { date } = this.state;
+    firebase
+      .database()
+      .ref("Calendar Page")
+      .child(date.slice(4, 8) + "_" + date.slice(2, 4) + "_" + date.slice(0, 2))
+      .child("Day Rating")
+      .set(event.target.value);
   }
 
   renderDatePage() {
-    const {date} = this.state;
+    const { date } = this.state;
     return (
-      <div className = "date">
+      <div className="date">
         <header>
-            <h1>
-              <center>{date.slice(0,2)+"/"+date.slice(2,4)+"/"+date.slice(4,8)}</center>
-           </h1>
+          <h1>
+            <center>{date.slice(0, 2) + "/" + date.slice(2, 4) + "/" + date.slice(4, 8)}</center>
+          </h1>
         </header>
-        <main>
-          <TaskList type="Mental" position="leftBlock" props={this.state}/>
-          <TaskList type="Physical" position="leftMiddleBlock" props={this.state}/>
-          <TaskList type="Health" position="rightMiddleBlock" props={this.state}/>
-          <TaskList type="Long Term Goals" position="rightBlock" props={this.state}/>
-        </main>
+        <TaskList type="Mental" position="leftBlock" props={this.state} />
+        <TaskList type="Physical" position="leftMiddleBlock" props={this.state} />
+        <TaskList type="Health" position="rightMiddleBlock" props={this.state} />
+        <TaskList type="Long Term Goals" position="rightBlock" props={this.state} />
         <center>
-        <div className="options"onChange={this.getOptionPicked.bind(this)}>
-          <input className="option" type="radio" name="pick" value="firstOption"/><b style={{color:"black"}}>x</b>
-          <input className="option" type="radio" name="pick" value="secondOption"/><b style={{color:"red"}}>x</b>
-          <input className="option" type="radio" name="pick" value="thirdOption"/><label style={{color:"black"}}>✓</label>
-          <input className="option" type="radio" name="pick" value="fourthOption"/><label style={{color:"red"}}>✓</label>
-          <input className="option" type="radio" name="pick" value="fifthOption"/><label style={{color:"red"}}><b>!</b></label>
-        </div>
+          <div className="optionsContainer" onChange={this.getOptionPicked.bind(this)}>
+            <div>
+              <input id="firstOption" type="radio" name="options"/>
+              <label className="black-label" htmlFor="firstOption"> x </label>
+            </div>
+            <div>
+              <input id="secondOption" type="radio" name="options"/>
+              <label className="red-label" htmlFor="secondOption"> x </label>
+            </div>
+            <div>
+              <input id="thirdOption" type="radio" name="options"/>
+              <label className="black-label" htmlFor="thirdOption"> ✓ </label>
+            </div>
+            <div>
+              <input id="fourthOption" type="radio" name="options"/>
+              <label className="red-label" htmlFor="fourthOption"> ✓ </label>
+            </div>
+            <div>
+              <input id="warningOption" type="radio" name="options"/>
+              <label className="red-label" htmlFor="warningOption"> ! </label>
+            </div>
+            <div>
+              <input id="clearOption" type="radio" name="options"/>
+              <label className="black-label" htmlFor="clearOption"> Clear </label>
+            </div>
+          </div>
         </center>
       </div>
     );
   }
 
   render() {
-      return this.renderDatePage();
+    return this.renderDatePage();
   }
 }
 
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,141 +1,187 @@
-import React from 'react';
-import firebase from '../Firebase';
+import React from "react";
+import firebase from "../Firebase";
 import Task from "./Task.js";
-import './styles/TaskList.css';
+import "./styles/TaskList.css";
 
 class TaskList extends React.Component {
-
   constructor(props) {
     super(props);
     this.state = {
-      type : this.props.type,
-      tasks: []
-    }
+      type: this.props.type,
+      tasks: [],
+    };
     this.input = React.createRef();
     this.readDatabase();
   }
 
   readDatabase() {
     const date = this.props.props.date;
-    const formattedDate = date.slice(4,8)+"_"+date.slice(2,4)+"_"+date.slice(0,2)
-    firebase.database().ref("Date Page").child(formattedDate).child(this.state.type)
-    .once("value", result => {
-      if (result.exists()) {
-        let tasks = result.val().slice(1,result.val().length)
-        let temp = tasks.map((task,index) => (
-           <Task key={index+1} content={Object.keys(task)[0]} completion={Object.values(task)[0]} />
-        ))
-        this.setState({
-          tasks:temp
-        })
-      }
-    })
+    const formattedDate = date.slice(4, 8) + "_" + date.slice(2, 4) + "_" + date.slice(0, 2);
+    firebase
+      .database()
+      .ref("Date Page")
+      .child(formattedDate)
+      .child(this.state.type)
+      .once("value", (result) => {
+        if (result.exists()) {
+          let tasks = result.val().slice(1, result.val().length);
+          let temp = tasks.map((task, index) => (
+            <Task
+              key={index + 1}
+              content={Object.keys(task)[0]}
+              completion={Object.values(task)[0]}
+            />
+          ));
+          this.setState({
+            tasks: temp,
+          });
+        }
+      });
   }
 
   writeDatabase() {
-    const {type,tasks} = this.state;
+    const { type, tasks } = this.state;
     const date = this.props.props.date;
-    const formattedDate = date.slice(4,8)+"_"+date.slice(2,4)+"_"+date.slice(0,2)
+    const formattedDate = date.slice(4, 8) + "_" + date.slice(2, 4) + "_" + date.slice(0, 2);
 
     firebase.database().ref("Date Page").child(formattedDate).child(type).remove();
-    tasks.map((task,index) => (
-      firebase.database().ref("Date Page").child(formattedDate).child(type)
-          .child(index+1).child(task.props.content).set(task.props.completion)
-    ));
+    tasks.map((task, index) =>
+      firebase
+        .database()
+        .ref("Date Page")
+        .child(formattedDate)
+        .child(type)
+        .child(index + 1)
+        .child(task.props.content)
+        .set(task.props.completion)
+    );
   }
 
-  removeTask(event,index) {
-    if(window.confirm('Are you sure you wish to delete this item?')) {
+  removeTask(event, index) {
+    if (window.confirm("Are you sure you wish to delete this item?")) {
       let tasks = this.state.tasks;
-      tasks.splice(index,1)
-      this.setState({
-        tasks:tasks
-      }, () => {
-        this.writeDatabase()
-      })
+      tasks.splice(index, 1);
+      this.setState(
+        {
+          tasks: tasks,
+        },
+        () => {
+          this.writeDatabase();
+        }
+      );
     }
   }
 
-  handleClick(event,index){
-      console.log("pressed");
-      console.log(event.target.checked);
-      let check = "incomplete";
-      if (event.target.checked) {
-        check = "complete"
+  handleClick(event, index) {
+    console.log("pressed");
+    console.log(event.target.checked);
+    let check = "incomplete";
+    if (event.target.checked) {
+      check = "complete";
+    }
+    let tasks = this.state.tasks;
+    let task = tasks[index];
+    tasks[index] = <Task key={index + 1} content={task.props.content} completion={check} />;
+    this.setState(
+      {
+        tasks: tasks,
+      },
+      () => {
+        this.writeDatabase();
       }
-      let tasks = this.state.tasks;
-      let task=tasks[index];
-      tasks[index]=<Task key={index+1} content={task.props.content} completion={check} />
-      this.setState({
-        tasks:tasks
-      }, () => {
-        this.writeDatabase()
-      })
-    }
+    );
+  }
 
   addTask(event) {
-    if (event.key !== "Enter") { return; }
+    if (event.key !== "Enter") {
+      return;
+    }
     if (event.target.value !== "") {
-      const newTasks = [...this.state.tasks,<Task key={this.state.tasks.length+1} content={event.target.value} completion="incomplete"/>]
-      this.setState({
-        tasks:newTasks
-      }, () => {
-        this.writeDatabase()
-        this.input.current.value =""
-      })
-    }else{
+      const newTasks = [
+        ...this.state.tasks,
+        <Task
+          key={this.state.tasks.length + 1}
+          content={event.target.value}
+          completion="incomplete"
+        />,
+      ];
+      this.setState(
+        {
+          tasks: newTasks,
+        },
+        () => {
+          this.writeDatabase();
+          this.input.current.value = "";
+        }
+      );
+    } else {
       alert("Please enter a task!");
     }
   }
 
-  dragStart =(event,task)=> {
+  dragStart = (event, task) => {
     this.draggedItem = task;
-    event.dataTransfer.effectAllowed="move"
-    event.dataTransfer.setData('text/html',event.target.parentNode)
+    event.dataTransfer.effectAllowed = "move";
+    event.dataTransfer.setData("text/html", event.target.parentNode);
+  };
 
-  }
-
-  dragOver = (e,index,draggedOverTask) => {
+  dragOver = (e, index, draggedOverTask) => {
     if (this.draggedItem === draggedOverTask) {
       return;
     }
-    let tasks = this.state.tasks.filter(x => x !== this.draggedItem);
-    tasks.splice(index,0,this.draggedItem)
-    this.setState({tasks})
-
-  }
+    let tasks = this.state.tasks.filter((x) => x !== this.draggedItem);
+    tasks.splice(index, 0, this.draggedItem);
+    this.setState({ tasks });
+  };
 
   dragEnd = () => {
     this.draggedIdx = null;
     this.writeDatabase();
-  }
-
+  };
 
   render() {
-
     return (
-        <div className="position" id={this.props.position}>
-          <h1>
-            <center>{this.state.type}</center>
-          </h1>
-          <div className="inner-container">
-            <ul>
-              {this.state.tasks.map((task,index) => (
-                <li className="list" key={index-1} draggable="true" onDragStart={e => this.dragStart(e,task)} onDragOver={e => this.dragOver(e,index,task)}
-                    onDragEnd={() => this.dragEnd()} >
-                    <input type="checkbox"
-                    checked={String("complete") === task.props.completion} onChange={() => {}}
-                    name="list" onDoubleClick= {e => this.removeTask(e,index)} onClick={e => this.handleClick(e,index)}/>
-                    {task} </li>
-              ))}
-            </ul>
-          </div>
-            <center>
-              <input className="inputForTask" type="text" name="task" size="20" autoComplete="off"
-              onKeyDown={e =>this.addTask(e)} ref={this.input}/>
-            </center>
+      <div className="position" id={this.props.position}>
+        <h1>
+          <center>{this.state.type}</center>
+        </h1>
+        <div className="inner-container">
+          <ul>
+            {this.state.tasks.map((task, index) => (
+              <li
+                className="list"
+                key={index - 1}
+                draggable="true"
+                onDragStart={(e) => this.dragStart(e, task)}
+                onDragOver={(e) => this.dragOver(e, index, task)}
+                onDragEnd={() => this.dragEnd()}
+              >
+                <input
+                  type="checkbox"
+                  checked={String("complete") === task.props.completion}
+                  onChange={() => {}}
+                  name="list"
+                  onDoubleClick={(e) => this.removeTask(e, index)}
+                  onClick={(e) => this.handleClick(e, index)}
+                />
+                {task}{" "}
+              </li>
+            ))}
+          </ul>
         </div>
-      );
+        <h1>
+          <input
+            className="inputForTask"
+            type="text"
+            name="task"
+            size="20"
+            autoComplete="off"
+            onKeyDown={(e) => this.addTask(e)}
+            ref={this.input}
+          />
+        </h1>
+      </div>
+    );
   }
 }
 
